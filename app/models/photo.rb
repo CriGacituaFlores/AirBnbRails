ce5ba78fcd5d8e621ce8aa98e54e8690c9c1c5c2
Cristian <cgacitua@webdox.cl>
2016-11-02 23:20:29 -0300

    Add new model photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :room
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
