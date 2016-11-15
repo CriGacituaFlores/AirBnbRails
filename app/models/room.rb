b9f1b0fa10bb08e21902d352983c59419c3535cc
Cristian <cgacitua@webdox.cl>
2016-11-15 12:42:27 -0300

    Add model review

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
